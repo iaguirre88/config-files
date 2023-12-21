4db618a73b180841431fc48ce5e65c7f39fd5178
Ignacio Aguirrezabal <ignacio@wyeworks.com>
2023-12-21 10:51:01 -0300

    Update tmux config

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 ln -s $(pwd)/nvim ~/.config/nvim
 ln -s $(pwd)/zsh ~/.config/zsh
-ln -s $(pwd)/tmux.conf ~/.tmux.conf
+ln -s $(pwd)/tmux ~/.config/tmux
 ln -s $(pwd)/starship.toml ~/.config
 
 cp zsh/zshrc.example ~/.zshrc
